b378906f542e293d599633e912deac79647afd6f
Runkana Durga Prasad <runkanaprasad1@gmail.com>
2024-07-30 06:44:08 +0530

    removed uploader

--- app/uploaders/cover_image_uploader.rb
+++
@@ -1,9 +0,0 @@
-class CoverImageUploader < CarrierWave::Uploader::Base
-  include Cloudinary::CarrierWave
-
-  # Optional: You can add process or other configurations here
-  process convert: 'png'
-
-  # Use Cloudinary for storage
-  storage :cloudinary
-end
